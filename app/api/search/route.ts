35e098fd3960209caf12a7d0ff985d5e1723fb03
adamdgit <adam_d@outlook.com>
2024-08-06 00:12:21 +1000

    setting up api route handling

---
+++ app/api/search/route.ts
@@ -0,0 +1,26 @@
+import { createClient } from '@/utils/supabase/server';
+import { NextResponse, type NextRequest } from 'next/server'
+ 
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query')
+  console.log(query)
+
+
+  const supabase = createClient();
+  const { data: recipes, error: recipeError } = await supabase.from("recipes")
+    .select("name")
+    .like('name', `%${query}%`);
+
+  if (recipeError) return NextResponse.error();
+
+  // query is "hello" for /api/search?query=hello
+  return NextResponse.json({res: recipes})
+}
+
+// handle search results from form
+export async function POST(request: Request) {
+  const formData = await request.formData()
+  const search = formData.get('search')
+  return Response.json({ search })
+}
