b56a143d996d9f6c71ec69d3867037af5e5deb71
adamdgit <adam_d@outlook.com>
2024-08-06 00:14:52 +1000

    Navigation component for header navbar

---
+++ components/Navigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav className="navbar">
+      <ul className='nav-buttons'>
+        <li><a href=''>Home</a></li>
+        <li><a href=''>Top</a></li>
+        <li><a href=''>About</a></li>
+      </ul>
+    </nav>
+  )
+}
