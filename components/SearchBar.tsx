1f828ac2f6570b3f93c2139982a45837b35e4412
adamdgit <adam_d@outlook.com>
2024-08-06 00:19:46 +1000

    send form search query to search page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,19 @@
+'use client'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SearchBar() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
-    <form method='post'>
+    <form method='post' action={`http://localhost:3000/search?query=${searchQuery}`}>
       <label className='search-label' htmlFor='searchbar'>Search</label>
       <span className='searchbar'>
-        <input className='search-input' name='searchbar' type='search' />
-        <button className='search-btn'>
+        <input className='search-input' name='searchbar' type='search' onChange={(e) => setSearchQuery(e.target.value)} />
+        <button className='search-btn' type='submit'>
           <FontAwesomeIcon icon={faSearch} style={{width: '20px'}} />
         </button>
       </span>
