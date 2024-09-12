b73a53a29c20bcc8767a251552c7889ea1f93f12
adamdgit <adam_d@outlook.com>
2024-09-12 19:12:16 +1000

    Created types file to store exported types

---
+++ types/main.ts
@@ -0,0 +1,16 @@
+export type ErrorMsg = {
+  status: number,
+  msg: string
+}
+
+export type AssistantResponse = {
+  success: boolean,
+  recipe: {
+    name: string,
+    description: string,
+    tags: string[], // multiple tags
+    cooking_time: string, // eg: 1 hour 30 mins
+    ingredients: string[], // List of ingredients, also includes measurement eg: 2 eggs
+    instructions: string[] // list of instructions
+  }
+}
