22ddd8baf03aecb7174c306fe17b9e6fa48d6f52
adamdgit <adam_d@outlook.com>
2024-09-12 19:11:27 +1000

    replaced markdown with consistent json response

--- components/Transcribe.tsx
+++ components/Transcribe.tsx
@@ -1,21 +1,18 @@
 'use client'
 
 import React, { useState } from "react";
-import Markdown from 'react-markdown'
-
-type ErrorMsg = {
-  status: number,
-  msg: string
-}
+import { parse, Allow } from "partial-json";
+import type { ErrorMsg, AssistantResponse } from "@/types/main";
 
 export default function Transcribe() {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState("");
+  const [data, setData] = useState<AssistantResponse>();
   const [error, setError] = useState(false);
   const [errorMsg, setErrorMsg] = useState<ErrorMsg | undefined>();
 
   async function handleStreaming(reader: ReadableStreamDefaultReader<Uint8Array>, decoder: TextDecoder) {
+    let tmp = ''
     while (true) {
       const { done, value } = await reader.read();
       if (done) break;
@@ -23,13 +20,24 @@ export default function Transcribe() {
       // decodes unit8array into a string
       const chunk = decoder.decode(value, { stream: true });
       // append chunks to the string as they stream in
-      setData(prev => prev += chunk)
+      tmp += chunk;
+      let parsedJSON = parse(tmp);
+
+      // transcript is availalbe, but video is not a recipe
+      if (parsedJSON?.success === false) {
+        setError(true);
+        setErrorMsg({status: 400, msg: "No recipe found in video"});
+        return
+      }
+
+      setData(parsedJSON)
     }
   }
 
-  async function handleOpenAIAPI() {
+  async function fetchRecipeTranscription() {
     setLoading(true);
-
+    setError(false);
+    
     const response = await fetch(`http://localhost:3000/api/transcribe?url=${url}`, {
       method: "POST",
       headers: {
@@ -49,6 +57,16 @@ export default function Transcribe() {
     }
   }
 
+  async function saveRecipe() {
+    const response = await fetch(`http://localhost:3000/api/recipe/create`, {
+      method: "POST",
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({...data, "shorts_url": url})
+    });
+  }
+
   return (
     <div className="content-wrap">
       <div className="url-wrap">
@@ -58,14 +76,33 @@ export default function Transcribe() {
           placeholder="example: https://www.youtube.com/shorts/[video-id]" 
           onChange={(e) => setUrl(e.target.value)}
         />
-        <button className="generate-btn" onClick={() => handleOpenAIAPI()}>Generate</button>
+        <button className="generate-btn" onClick={() => fetchRecipeTranscription()}>Generate</button>
       </div>
 
       {loading && <p>Connecting to AI assistant..</p>}
 
-      {error && <p>{errorMsg?.msg}</p>}
+      {error ?  
+        <p>{errorMsg?.msg}</p>
+      : data?.recipe &&
+        <div className={"streamed-content"}>
+          <h3>Name: {data?.recipe.name}</h3>
+          <p><strong>Description: </strong> {data?.recipe.description}</p>
+          <p><strong>Cooking Time: </strong>{data?.recipe.cooking_time}</p>
+          <strong>Tags</strong>
+          <ul style={{display: 'flex', gap: "1rem"}}>
+            {data?.recipe.tags?.map((tag, i) => <li key={i}>{tag}</li>)}
+          </ul>
+          <strong>Ingredients</strong>
+          <ul>
+            {data?.recipe.ingredients?.map((ingr, i) => <li key={i}>{ingr}</li>)}
+          </ul>
+          <strong>Instructions</strong>
+          <ol>
+            {data?.recipe.instructions?.map((inst, i) => <li key={i}>{inst}</li>)}
+          </ol>
 
-      {data && <Markdown className={"streamed-content"}>{data}</Markdown>}
+          <button onClick={() => saveRecipe()}>Save Recipe</button>
+        </div>}
 
     </div>
   )
