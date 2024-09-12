8ade48dc0cb233074854f9a1970c82adc18c98b0
adamdgit <adam_d@outlook.com>
2024-09-12 19:05:45 +1000

    no changes?

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,20 @@ import { createClient } from '@/utils/supabase/server';
 export default async function Page() {
 
   const supabase = createClient();
-  const { data: recipes, error: recipeError } = await supabase.from("recipes")
+  const { data: recipes, error } = await supabase.from("recipes")
     .select("*")
     .gte("rating", 4)
+    .order("rating", { ascending: false })
     .limit(20);
       
   return (
     <div className='content-wrap'>  
       <h1>Top Recipes</h1>
 
-      {recipeError 
+      {error 
         ? <span className='supabase-error'>Something went wrong</span>
-        : <Results data={recipes} />}
+        : <Results data={recipes} />
+      }
     </div>
   );
 }
