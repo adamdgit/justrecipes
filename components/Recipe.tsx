bf7a6e3f8c770a0501da39e2f0f07c5b9e1928bf
adamdgit <adam_d@outlook.com>
2024-09-12 19:09:48 +1000

    Improved recipe component, added save button and ratings

--- components/Recipe.tsx
+++ components/Recipe.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { createClient } from '@/utils/supabase/server';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import Rating from './Rating';
+import SaveRecipe from './SaveRecipe';
 
 export default async function Recipe({ id }: { id: string }){
 
   const supabase = createClient();
+  const { data: { user } } = await supabase.auth.getUser();
+
   const { data, error: recipeError } = await supabase.from("recipes")
     .select("*")
     .eq('id', Number(id));
@@ -15,31 +21,40 @@ export default async function Recipe({ id }: { id: string }){
 
   return (
     <React.Fragment>
-    <h3>{recipe.name}</h3>
-    <div className='recipe-full'>
-      <div className='recipe-left-half'>
-        <img src={recipe.preview_img_url ?? ''} alt={recipe.name + ' preview image'} className='recipe-img' />
-        <span>{recipe.description}</span>
-
-        <span className='heading-small'>Ingredients: </span>
-        <ul className='ingredients-list'>
-          {recipe.ingredients?.split("\n").map(ingredient => {
-            return <li>{ingredient}</li>
-          })}
-        </ul>
-
-        <span className='heading-small'>Instructions: </span>
-        <ol className='instructions-list'>
-          {recipe.instructions?.split('\n').map(instruction => {
-            return <li>{instruction}</li>
-          })}
-        </ol>
+      <div className='recipe-like'>
+        <h2>{recipe.name}</h2>
+        <SaveRecipe id={id} user_ID={user?.id ?? ""} />
       </div>
 
-      <div className='recipe-right-half'>
-        <iframe width="320" height="640" src={`https://www.youtube.com/embed/${recipe?.shorts_url}` ?? 'no video found'} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <div className='recipe-full'>
+        <div className='recipe-left-half'>
+          <Rating rating={recipe.rating ?? 0} /> 
+          <span className='cooking-time'>
+            Cooking Time: 
+            <FontAwesomeIcon icon={faClock} width={20} height={20} />
+            {recipe.cooking_time}
+          </span>
+          <span>{recipe.description}</span>
+
+          <span className='heading-small'>Ingredients: </span>
+          <ul className='ingredients-list'>
+            {recipe.ingredients?.split("\n").map((ingredient, i) => {
+              return <li key={i}>{ingredient}</li>
+            })}
+          </ul>
+
+          <span className='heading-small'>Instructions: </span>
+          <ol className='instructions-list'>
+            {recipe.instructions?.split('\n').map((instruction, i) => {
+              return <li key={i}>{instruction}</li>
+            })}
+          </ol>
+        </div>
+
+        <div className='recipe-right-half'>
+          <iframe width="320" height="640" src={`https://www.youtube.com/embed/${recipe?.shorts_url}` ?? 'no video found'} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
       </div>
-    </div>
     </React.Fragment>
   )
 }
