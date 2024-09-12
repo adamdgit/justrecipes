17ed3543022e2828f13e18566249315194d25a1f
adamdgit <adam_d@outlook.com>
2024-09-12 19:06:01 +1000

    Added create recipe endpoint

---
+++ app/api/recipe/create/route.ts
@@ -0,0 +1,44 @@
+import { type NextRequest } from 'next/server'
+import { NextApiResponse } from 'next';
+import { createClient } from '@/utils/supabase/server';
+import { getIDfromURL } from '@/utils/getIDfromURL';
+
+export async function POST(request: NextRequest, res: NextApiResponse) {
+  const body = await request.json()
+  const URL_ID = getIDfromURL(body?.shorts_url);
+
+  const supabase = createClient();
+  const { data, error } = await supabase.from("recipes")
+    .insert([
+      {
+        rating: null, 
+        rating_count: null,
+        shorts_url: URL_ID,
+        preview_img_url: `https://img.youtube.com/vi/${URL_ID}/hqdefault.jpg`,
+        serves: null,
+        name: body.recipe.name,   
+        description: body.recipe.description, 
+        // add newline chars to each string in the array before saving in database, 
+        // don't add a newline for last item
+        ingredients: body.recipe.ingredients
+          .map((x: string, idx: number) => 
+            idx !== body.recipe.ingredients.length -1 ? x.concat("\n") : x
+          ).join(''),
+        instructions: body.recipe.instructions
+          .map((x: string, idx: number) => 
+            idx !== body.recipe.instructions.length -1 ? x.concat("\n") : x
+          ).join(''),
+        cooking_time: body.recipe.cooking_time,
+      }
+    ])
+  
+  console.log(error)
+  console.log(data)
+
+  if (error) {
+    return new Response(null, { statusText: error.message, status: 401 });
+  }
+    
+  // handle invalid urls
+  return new Response(null, { statusText: "Recipe created successfully", status: 200 });
+}
