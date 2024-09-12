9530d1234ab02379caaed7997aa00317df8f5cb5
adamdgit <adam_d@outlook.com>
2024-09-12 19:13:04 +1000

    Check if a given URL is valid

--- utils/isValidURL.ts
+++ utils/isValidURL.ts
@@ -1,7 +1,15 @@
+/**
+ * Validates if a given URL is a valid youtube link.
+ * @param {string} url - The full URL.
+ * @returns {boolean} true or false.
+ * @example
+ * isValidURL("https://www.youtube.com/shorts/abc123?si=example");
+ * // returns 'true'
+ */
 export function isValidURL(url: string) {
   if (!url || url.length === 0) return false
-  if (!url.startsWith("https://youtube.com")) return false
-  if (!url.startsWith("https://www.youtube.com")) return false
-  if (!url.includes("/shorts")) return false
-  return true
+  if (url.startsWith("https://youtube.com")) return true
+  if (url.startsWith("https://www.youtube.com")) return true
+  if (url.startsWith("https://youtu.be")) return true
+  return false
 }
