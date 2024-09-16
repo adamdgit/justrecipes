203fec6c2794d196cb93a8f3dbaf8f61153b78c9
adamdgit <adam_d@outlook.com>
2024-09-16 20:23:50 +1000

    Completed save functionality, saves recipes

--- components/SaveRecipe.tsx
+++ components/SaveRecipe.tsx
@@ -1,34 +1,88 @@
 'use client'
 
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { ErrorMsg } from '@/types/main'
+import { faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 
 type Props = {
   id: string, 
-  user_ID: string
+  user_ID: string | null,
+  isSaved: boolean
 }
 
-export default function SaveRecipe({ id, user_ID }: Props) {
+export default function SaveRecipe({ id, user_ID, isSaved }: Props) {
 
-  const [error, setError] = useState();
+  const [errorMsg, setErrorMsg] = useState<ErrorMsg>({ msg: null, status: null });
+  const [userHasSaved, setUserHasSaved] = useState(isSaved);
+  const [isHovered, setIsHovered] = useState(false);
 
   async function handleSaveRecipe() {
-    const result = await fetch(`http://localhost:3000/api/recipe/save?recipeid=${id}&userid=${user_ID}`,
-      {
-        method: "POST"
+    setErrorMsg({msg: null, status: null})
+
+    if (!userHasSaved) {
+      const response = await fetch(`http://localhost:3000/api/recipe/save?recipeid=${id}&userid=${user_ID}`,
+        {
+          method: "POST"
+        }
+      );
+  
+      if (!response.ok) {
+        setErrorMsg({msg: response.statusText, status: response.status})
+      } else {
+        setUserHasSaved(true)
       }
-    );
-    const data = await result.json(); // success response
+    }
+
+    if (userHasSaved) {
+      const response = await fetch(`http://localhost:3000/api/recipe/unfollow?recipeid=${id}&userid=${user_ID}`,
+        {
+          method: "POST"
+        }
+      );
+  
+      if (!response.ok) {
+        setErrorMsg({msg: response.statusText, status: response.status})
+      } else {
+        setUserHasSaved(false)
+      }
+    }
   }
 
-  return (
-    <div>
-      <FontAwesomeIcon 
-        onClick={() => handleSaveRecipe()} 
-        icon={faHeart} 
-        width={'20px'} height={'20px'} 
-      />
-    </div>
-  )
+  if (userHasSaved) {
+    return (
+      <button className='save-btn'
+        onClick={() => handleSaveRecipe()}  
+        onPointerEnter={() => setIsHovered(true)}
+        onPointerLeave={() => setIsHovered(false)}
+      >
+        {isHovered ? 
+          <FontAwesomeIcon icon={faHeartBroken} />
+          :        
+          <FontAwesomeIcon 
+            icon={faHeart} 
+            width={20} height={20} 
+            color='var(--color1)'
+          />
+        }
+       
+        Unfollow Recipe
+      </button>
+    )
+  }
+
+  if (!userHasSaved) {
+    return (
+      <button className='save-btn'
+        onClick={() => handleSaveRecipe()}
+      >
+        <FontAwesomeIcon 
+          icon={faHeart} 
+          width={20} height={20} 
+          fill='currentColor'
+        />
+        Save recipe
+      </button>
+    )
+  }
 }
