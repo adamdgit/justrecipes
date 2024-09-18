8b718c3f2bd50a9e843cc02d25deecfc26bbb14d
adamdgit <adam_d@outlook.com>
2024-09-18 17:37:08 +1000

    Updated AssistantResponse types to include categories

--- types/main.ts
+++ types/main.ts
@@ -1,16 +1,60 @@
 export type ErrorMsg = {
-  status: number | null,
-  msg: string | null
+  status: number | null;
+  msg: string | null;
 }
 
 export type AssistantResponse = {
-  success: boolean,
+  success: boolean;
+  video_url: string;
   recipe: {
-    name: string,
-    description: string,
-    tags: string[], // multiple tags
-    cooking_time: string, // eg: 1 hour 30 mins
-    ingredients: string[], // List of ingredients, also includes measurement eg: 2 eggs
-    instructions: string[] // list of instructions
-  }
-}
+    name: string;
+    description: string;
+    cooking_time: string;
+    categories: (
+      | "Cakes"
+      | "Pastry"
+      | "Dessert"
+      | "Breakfast"
+      | "Lunch"
+      | "Dinner"
+      | "Pies"
+      | "Sweet"
+      | "Biscuits"
+      | "Ice Cream"
+      | "Beverage"
+      | "Alcohol"
+      | "Soups"
+      | "Burgers"
+      | "Candy"
+      | "Bread"
+      | "Fruit"
+      | "Vegetable"
+      | "Protein"
+      | "Pasta"
+      | "Smoothie"
+      | "Stews"
+      | "Slow Cook"
+      | "BBQ"
+      | "Salad"
+      | "Muffins"
+      | "Roasts"
+      | "Gluten-Free"
+      | "Dairy-Free"
+      | "Nut-Free"
+      | "Vegan"
+      | "Vegetarian"
+      | "Carnivore"
+      | "Chicken"
+      | "Red Meat"
+      | "Rice"
+      | "Pork"
+      | "Poultry"
+      | "Seafood"
+      | "Air-Fry"
+      | "Snacks"
+      | "Pizzas"
+    )[]; // An array containing exactly 2 categories from the predefined list
+    ingredients: string[]; // List of ingredients, also includes measurements (e.g., '2 eggs')
+    instructions: string[]; // List of instructions
+  };
+};
