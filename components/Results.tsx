416154e92221191207c8f6d9f5d60d5201ddfa7e
adamdgit <adam_d@outlook.com>
2024-09-18 17:35:57 +1000

    no longer store img url in database, only need video id

--- components/Results.tsx
+++ components/Results.tsx
@@ -15,7 +15,7 @@ export default async function Results({ data } : ResultsProps) {
       <a href={`http://localhost:3000/recipes/${recipe.id}`} 
         key={recipe.id}
         className='result-link'>
-        <img src={recipe.preview_img_url ?? ''} alt={recipe.name + ' preview image'} className='recipe-img' />
+        <img src={`https://img.youtube.com/vi/${recipe.video_id}/hqdefault.jpg` ?? ''} alt={recipe.name + ' preview image'} className='recipe-img' />
         <div className='result-text'>
           <h3>{recipe.name}</h3>
           <p>{recipe.description}</p>
@@ -23,7 +23,9 @@ export default async function Results({ data } : ResultsProps) {
           <Rating 
             user_id={null}
             rating={recipe.rating ?? 0} 
+            user_rating={0}
             count={recipe.rating_count ?? 0} 
+            recipe_id={recipe.id}
           />
         </div>
       </a>)}
