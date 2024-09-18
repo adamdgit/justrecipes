30b2ac797c160c1e8b459f30fe41b1b5b9742b0c
adamdgit <adam_d@outlook.com>
2024-09-18 17:35:11 +1000

    Added rating modal for logged in users to rate recipes

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -9,28 +9,30 @@ import React from 'react';
 type Props = {
   user_id: string | null,
   rating: number,
-  count: number
+  user_rating: number,
+  count: number,
+  recipe_id: number
 }
 
-export default function Rating({ user_id, rating, count } : Props) {
+export default function Rating({ user_id, rating, user_rating, count, recipe_id } : Props) {
+  // calculate how many of each star exists, to then map over
   const fullStars = Math.floor(rating);
-  const halfStars = (rating % 1 >= 0.5) ? 1 : 0; // return 1 if there is a half star
+  const halfStars = (rating % 1 >= 0.5) ? 1 : 0;
   const emptyStars = 5 - fullStars - halfStars;
 
   const [showModal, setShowModal] = useState(false);
   const [userRating, setUserRating] = useState(0);
 
   async function handleUserRating(num: number) {
-    if (!user_id) return
+    if (!user_id || user_rating > 0) return
 
-    const response = await fetch(`http://localhost:3000/api/recipe/rate?user_id=${user_id}&rating=${num}`,
-      {
-        method: "POST"
-      }
+    const response = await fetch(
+      `http://localhost:3000/api/recipe/rate?userid=${user_id}&rating=${num}&recipeid=${recipe_id}`,
+      { method: "POST" }
     );
 
     if (!response.ok) {
-
+      // TODO update rating state, user has rated
     } else {
 
     }
@@ -51,7 +53,7 @@ export default function Rating({ user_id, rating, count } : Props) {
             <FontAwesomeIcon key={i} icon={starOutline} />
           ))}
         </div>
-        <span>{count} votes</span>
+        {user_id ? <span>{count} votes</span> : null}
 
         {user_id ? 
           <button className='btn' onClick={() => setShowModal(!showModal)}>
@@ -60,7 +62,7 @@ export default function Rating({ user_id, rating, count } : Props) {
         : null}
       </div>
 
-      {showModal ? <div className='rating-modal'>
+      {showModal && !user_rating ? <div className='rating-modal'>
         Give a rating:
         <div>
           {Array(5).fill(0).map((_, i) => (
@@ -75,7 +77,21 @@ export default function Rating({ user_id, rating, count } : Props) {
             />
           ))}
         </div>
-      </div> : null}
+      </div> 
+      : showModal && <div className='rating-modal'>
+      You rated:
+      <div>
+        {Array(5).fill(0).map((_, i) => (
+          <FontAwesomeIcon 
+            key={i} 
+            width={20}
+            height={20}
+            icon={user_rating >= (i +1) ? starSOlid : starOutline} 
+            style={user_rating >= (i +1) ? {color: "var(--color1)", cursor: 'pointer'} : {cursor: 'pointer'}}
+          />
+        ))}
+      </div>
+    </div>}
     </React.Fragment>
   )
 }
