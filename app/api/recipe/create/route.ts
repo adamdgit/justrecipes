6c88857f34480722e8807f5b32801f1756f00303
adamdgit <adam_d@outlook.com>
2024-09-18 17:33:25 +1000

    Added categories table insert

--- app/api/recipe/create/route.ts
+++ app/api/recipe/create/route.ts
@@ -2,19 +2,19 @@ import { type NextRequest } from 'next/server'
 import { NextApiResponse } from 'next';
 import { createClient } from '@/utils/supabase/server';
 import { getIDfromURL } from '@/utils/getIDfromURL';
+import { AssistantResponse } from '@/types/main';
 
 export async function POST(request: NextRequest, res: NextApiResponse) {
-  const body = await request.json()
-  const URL_ID = getIDfromURL(body?.shorts_url);
+  const body: AssistantResponse = await request.json();
+  const URL_ID = getIDfromURL(body?.video_url);
 
   const supabase = createClient();
-  const { data, error } = await supabase.from("recipes")
+  const { data, error: recipeError } = await supabase.from("recipes")
     .insert([
       {
-        rating: null, 
+        rating: null,
         rating_count: null,
-        shorts_url: URL_ID,
-        preview_img_url: `https://img.youtube.com/vi/${URL_ID}/hqdefault.jpg`,
+        video_id: URL_ID,
         serves: null,
         name: body.recipe.name,   
         description: body.recipe.description, 
@@ -31,13 +31,43 @@ export async function POST(request: NextRequest, res: NextApiResponse) {
         cooking_time: body.recipe.cooking_time,
       }
     ])
-    .select()
-  
-  console.log(error)
-  console.log(data)
+    .select();
 
-  if (error) {
-    return new Response(null, { statusText: error.message, status: 401 });
+  if (recipeError) {
+    return new Response(null, { statusText: recipeError.message, status: 401 });
+  }
+
+  // get categories ID's from the categories table
+  const { data: categoryData, error: categoryError } = await supabase.from('categories')
+    .select('id')
+    .in('name', body.recipe.categories);
+
+  if (categoryError) {
+    // roll back inserted data if something goes wrong
+    await supabase.from('recipes')
+      .delete()
+      .eq('id', data[0].id);
+
+    return new Response(null, { statusText: categoryError.message, status: 401 });
+  }
+
+  // creating relational data array for recipe_categories table
+  const recipeCategories = categoryData.map(category => ({
+    recipe_id: data[0].id,
+    category_id: category.id
+  }));
+
+  // insert categories by ID, related to this newly created recipe
+  const { error: recipeCategoriesError } = await supabase.from('recipe_categories')
+    .insert(recipeCategories);
+  
+  if (recipeCategoriesError) {
+    // roll back inserted data if something goes wrong
+    await supabase.from('recipes')
+    .delete()
+    .eq('id', data[0].id);
+
+    return new Response(null, { statusText: recipeCategoriesError.message, status: 401 });
   }
 
   // return item ID to use on frontend for displaying link to the newly added item
