ab3d929dfc5eea942dc9672d409fadbd24cb6d51
adamdgit <adam_d@outlook.com>
2024-09-22 17:04:04 +1000

    content wrap div moved to layout

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -1,12 +1,11 @@
 import Recipe from "@/components/Recipe";
+import React from "react";
 
 export default function Page({ params }: { params: { id: string } }) {
 
   return (
-    <div className='content-wrap'>
-      <Recipe 
-        id={params.id}
-      />
-    </div>
+    <Recipe 
+      id={params.id}
+    />
   )
 }
