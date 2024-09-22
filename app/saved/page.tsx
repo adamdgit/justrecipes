885703607be43a4ba20b665488a5eaa9326b24f4
adamdgit <adam_d@outlook.com>
2024-09-23 01:55:49 +1000

    Cleaned up code

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -14,22 +14,20 @@ export default async function page() {
 
   const { data, error } = await supabase.from('user_saved_recipes')
   .select(`
-    recipes (
-      *
-    )
+    recipes (*)
   `)
   .eq('user_id', user.id);
     
-  const savedRecipes = data ? data.flatMap(x => x.recipes ? [x.recipes] : []) : null;
+  const savedRecipes = data ? data.flatMap(x => x.recipes ? [x.recipes] : []) : [];
 
   if (error) return (
-    <ErrorMessage msg={"Error retrieving saved recipes"} />
+    <ErrorMessage msg={"Error retrieving saved recipes"} needsUpdate={true} />
   )
 
   return (
     <React.Fragment>
       <h1>Saved Recipes</h1>
-     <Results data={savedRecipes} />
+     {savedRecipes?.length > 0 ? <Results data={savedRecipes} /> :<p>No saved recipes</p>}
     </React.Fragment>
   )
 }
