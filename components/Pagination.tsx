6c91c01eaee3e5b8481efb1ed734ab936ba714e5
adamdgit <adam_d@outlook.com>
2024-09-23 01:57:00 +1000

    Removed pagination numbers, just has back and next buttons

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -6,47 +6,36 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 
 type Props = {
-  start: number, 
-  end: number,
+  start: number,
   searchQuery: string 
 }
 
-export default function Pagination({ start, end, searchQuery }: Props) {
+export default function Pagination({ start, searchQuery }: Props) {
   const router = useRouter();
   const [startIndex, setStartIndex] = useState(start);
 
   // if client manually alters the url, this updates url to correct values from server
   useEffect(() => {
-    router.push(`http://localhost:3000/search?query=${searchQuery}&start=${start}&end=${end}`)
+    router.push(`http://localhost:3000/search?query=${searchQuery}&start=${start}`)
   },[])
   
   return (
     <div className='pagination-wrap'>
       <ul className='pagination'>
         <li>
-          <a 
-            href={ `http://localhost:3000/search?query=${searchQuery}&start=${startIndex}&end=${startIndex+20}`}
+          <a className='btn'
+            href={ `http://localhost:3000/search?query=${searchQuery}&start=${startIndex}`}
             onClick={() => setStartIndex(startIndex - 20 < 0 ? 0 : startIndex - 20)}
           >
           <FontAwesomeIcon icon={faChevronLeft} />
+          Back
          </a>
         </li>
-        {Array(4).fill(0).map((_, i) => {
-          return <li key={i}>
-            <a href={
-              `http://localhost:3000/search?query=${searchQuery}&start=${startIndex}&end=${startIndex+20}`
-            } 
-            style={start / 20 === i ? {color: "var(--color1)"} : {}}
-            onClick={() => setStartIndex((i) * 20)}>
-              {i +1}
-            </a>
-          </li>
-        })}
         <li>
-          <a 
-            href={ `http://localhost:3000/search?query=${searchQuery}&start=${startIndex}&end=${startIndex+20}`}
+          <a className='btn'
+            href={ `http://localhost:3000/search?query=${searchQuery}&start=${startIndex}`}
             onClick={() => setStartIndex(startIndex + 20)}
-          >
+          >Next
             <FontAwesomeIcon icon={faChevronRight} />
           </a>
         </li>
