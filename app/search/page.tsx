ab92255ef1a16f385421ec3c8f7d8f5266e4dc59
adamdgit <adam_d@outlook.com>
2024-09-22 17:06:43 +1000

    Added pagination support

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,7 @@
-import Results from '@/components/Results';
+import Pagination from '@/components/Pagination';
+import SearchFilter from '@/components/SearchFilter';
 import { createClient } from '@/utils/supabase/server';
+import React from 'react';
 
 export default async function page({ params, searchParams,}: 
 {
@@ -7,22 +9,39 @@ export default async function page({ params, searchParams,}:
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
 
-  const query = searchParams?.query;
+  const query = searchParams?.query as string;
+  let start = Number(searchParams?.start) ?? 0;
+  let end = Number(searchParams?.end) ?? 20;
+
+  // ensure end range is always +20 of the start
+  let remainder = start % 20;
+  if (remainder !== 0) start -= remainder; // start will always be multiple of 20
+  if (end <= start || end > start + 20) end = start + 20;
 
   const supabase = createClient();
   const { data: recipes, error } = await supabase.from("recipes")
-    .select("*")
+    .select(`*,
+      recipe_categories (category_id)
+    `)
     .ilike('name', `%${query}%`)
-    .limit(20);
+    .range(start, end);
     
-  if (error) return <span className='supabase-error'>Something went wrong</span>
+  if (error) return (
+    <span className='supabase-error'>Error fetching recipes</span>
+  )
+
+  const { data: categories, error: categoryError } = await supabase.from("categories")
+    .select("*");
+
+  if (categoryError) return (
+    <span className='supabase-error'>Error fetching categories</span>
+  )
 
   return (
-    <div className='content-wrap'>
+    <React.Fragment>
       <h1>Search Results:</h1>
-      {error 
-        ? <span className='supabase-error'>Something went wrong</span>
-        : <Results data={recipes} />}
-    </div>
+      <SearchFilter data={recipes} categories={categories} />
+      <Pagination start={start} end={end} searchQuery={query} />
+    </React.Fragment>
   )
 }
