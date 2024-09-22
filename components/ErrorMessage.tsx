0661065111a9b3a4d3dd07be1dff8daf5aafb0cd
adamdgit <adam_d@outlook.com>
2024-09-23 01:56:13 +1000

    Added self hiding logic to error component

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
 type Props = {
-  msg: string | null
+  msg: string | null,
+  needsUpdate: boolean
 }
 
-export default function ErrorMessage({ msg } : Props) {
+export default function ErrorMessage({ msg, needsUpdate } : Props) {
+  const [shouldUpdate, setShouldUpdate] = useState(needsUpdate);
+
+  // hide error message component after 2 seconds
+  useEffect(() => {
+    console.log("update")
+    setTimeout(() => {
+      setShouldUpdate(false)
+    }, 3500)
+  },[shouldUpdate])
+
   return (
-    <div className="error-msg">
+    <div className="error-msg" style={shouldUpdate ? {opacity: "1"} : {opacity: "0"}}>
       <p>{msg ? msg : "Something went wrong."}</p>
     </div>
   )
