552cc6926b78e14fad6cd46706bf2a1a5d220629
adamdgit <adam_d@outlook.com>
2024-09-22 17:03:16 +1000

    content wrap div moved to layout, replaced with fragment

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -2,6 +2,7 @@ import Helper from "@/components/Helper";
 import Transcribe from "@/components/Transcribe";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import React from "react";
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -12,11 +13,10 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className='content-wrap'>
-      <h1>Generate recipe from video using AI</h1>
-      <p><strong>Supports:</strong> Youtube videos and shorts</p>
+    <React.Fragment>
+      <h1>Generate a recipe from video using AI</h1>
       <Helper />
       <Transcribe />
-    </div>
+    </React.Fragment>
   );
 }
