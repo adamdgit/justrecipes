ac1a78c6981ad51b3b5954cb1ac2450fdb12bd4c
adamdgit <adam_d@outlook.com>
2024-09-27 18:47:08 +1000

    Replaced searchbar with form for useability

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,7 +6,6 @@ import React, { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 
 export default function SearchBar() {
-
   // get search query after redirect to search page if it exists
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
@@ -15,17 +14,20 @@ export default function SearchBar() {
 
   return (
     // default start and end for pagination
-      <span className='searchbar'>
-        <label className='search-label' htmlFor='searchbar'>Search</label>
-        <input className='search-input' 
-          placeholder='search existing recipes'
-          defaultValue={query ?? ''}
-          name='searchbar' 
-          type='search' 
-          onChange={(e) => setSearchQuery(e.target.value)} />
-        <a href={`http://localhost:3000/search?query=${searchQuery}&start=0&end=20`} className='search-btn' type='submit'>
-          <FontAwesomeIcon icon={faSearch} style={{width: '20px'}} />
-        </a>
-      </span>
+    <form method='post' 
+      action={`http://localhost:3000/search?query=${searchQuery}&start=0&end=20`}
+      className='searchbar'
+    >
+      <label className='search-label' htmlFor='searchbar'>Search</label>
+      <input className='search-input' 
+        placeholder='search existing recipes'
+        defaultValue={query ?? ''}
+        name='searchbar' 
+        type='search' 
+        onChange={(e) => setSearchQuery(e.target.value)} />
+      <button type='submit' className='search-btn'>
+        <FontAwesomeIcon icon={faSearch} style={{width: '20px'}} />
+      </button>
+    </form>
   )
 }
