2d6ff425018e51d64bbfe69e50915a4273eee5e0
adamdgit <adam_d@outlook.com>
2024-09-27 18:46:33 +1000

    Added simple loading spinner svg to loading component

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,7 +1,12 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export default function Loading(message?: string) {
+export default function Loading({ message }: { message?: string }) {
   return (
-    <div className='loading'>{message ? message : "loading..."}</div>
+    <div className='loading'>
+      <span>{message ? message : "loading..."}</span>
+      <FontAwesomeIcon icon={faSpinner} className='loading-spinner' />
+    </div>
   )
 }
